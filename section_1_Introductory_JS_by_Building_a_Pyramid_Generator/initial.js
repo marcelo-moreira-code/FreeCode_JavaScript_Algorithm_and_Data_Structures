b45b0c399e727dd267ecb74528314b6a25c388d2
Marcelo S. Moreira <contatomarcelomoreira.code@gmail.com>
2024-06-07 11:28:43 -0300

    Update initial.js

---
+++ section_1_Introductory_JS_by_Building_a_Pyramid_Generator/initial.js
@@ -0,0 +1,8 @@
+/*
+JS has seven primitive data types, with String being one of them. In JavaScript, a string represents a sequence of characters and can be enclosed in either single (') or double (") quotes.
+
+Note that strings are immutable, which means once they are created, they cannot be changed.
+*/
+
+let number = 10;
+console.log(number);
